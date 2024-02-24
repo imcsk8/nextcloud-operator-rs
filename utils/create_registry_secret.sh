dfc841f28b83a9782d986de3219d4a41c945201d
Iván Chavero <ivan.chavero@ugd.gob.mx>
2024-02-24 12:50:16 -0600

    Add namespace to secret creation

--- utils/create_registry_secret.sh
+++ utils/create_registry_secret.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-kubectl create secret generic $1 \
+kubectl create secret generic $1     \
+    -n $3                            \
     --from-file=.dockerconfigjson=$2 \
     --type=kubernetes.io/dockerconfigjson
