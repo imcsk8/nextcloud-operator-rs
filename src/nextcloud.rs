73c43238bb246e401ff9d8927bc5bdb7df806db3
Iván Chavero <ivan.chavero@ugd.gob.mx>
2024-02-26 23:57:26 -0600

    Refactor deployment

--- src/nextcloud.rs
+++ src/nextcloud.rs
@@ -9,109 +9,147 @@ use std::sync::Arc;
 use log::{info, debug};
 use sha2::{Digest, Sha256};
 
-/// Creates a new deployment of `n` pods with the `imcsk8/nextcloud:latest` docker image inside,
-/// where `n` is the number of `replicas` given.
-///
-/// # Arguments
-/// - `client` - A Kubernetes client to create the deployment with.
-/// - `name` - Name of the deployment to be created
-/// - `replicas` - Number of pod replicas for the Deployment to contain
-/// - `namespace` - Namespace to create the Kubernetes Deployment in.
-///
-/// Note: It is assumed the resource does not already exists for simplicity. Returns an `Error` if it does.
-pub async fn deploy(
+struct NextcloudElement {
+    name: String,
+    prefix: String,
+    image: String,
+    namespace: String,
+    replicas: i32,
+    container_port: i32,
+    labels: BTreeMap<String, String>,
+    image_pull_secrets: Vec<LocalObjectReference>,
+    annotations: BTreeMap<String, String>,
+}
+
+
+/// NextcloudElement implementation
+impl NextcloudElement {
+
+    /// returns a new deployment of `n` pods with the `imcsk8/nextcloud:latest` docker image inside,
+    /// where `n` is the number of `replicas` given.
+    ///
+    /// # Arguments
+    /// - `client` - A Kubernetes client to create the deployment with.
+    /// - `name` - Name of the deployment to be created
+    /// - `replicas` - Number of pod replicas for the Deployment to contain
+    /// - `namespace` - Namespace to create the Kubernetes Deployment in.
+    ///
+    pub fn as_deployment(&self) -> Result<Deployment, Error> {
+
+        Ok(Deployment {
+            metadata: ObjectMeta {
+                name: Some(self.name.to_owned()),
+                namespace: Some(self.namespace.to_owned()),
+                labels: Some(self.labels.clone()),
+                annotations: Some(self.annotations.clone()),
+                ..ObjectMeta::default()
+            },
+            spec: Some(DeploymentSpec {
+                replicas: Some(self.replicas),
+                selector: LabelSelector {
+                    match_expressions: None,
+                    match_labels: Some(self.labels.clone()),
+                },
+                template: PodTemplateSpec {
+                    spec: Some(PodSpec {
+                        containers: vec![
+                            Container {
+                                name: format!("{}-{}", self.prefix, self.name).to_string(),
+                                image: Some(self.image.clone()),
+                                ports: Some(vec![ContainerPort {
+                                    container_port: self.container_port,
+                                    ..ContainerPort::default()
+                                }]),
+                            ..Container::default()
+                            },
+                        ],
+                        image_pull_secrets: Some(self.image_pull_secrets.clone()),
+                        ..PodSpec::default()
+                    }),
+                    metadata: Some(ObjectMeta {
+                        labels: Some(self.labels.clone()),
+                        ..ObjectMeta::default()
+                    }),
+                },
+                ..DeploymentSpec::default()
+            }),
+            ..Deployment::default()
+        })
+    }
+}
+
+pub async fn apply(
     client: Client,
     name: &str,
     nextcloud_object: Arc<Nextcloud>,
     namespace: &str,
-) -> Result<Deployment, Error> {
+) -> Result<(), Error> {
 
-    info!("Deploying: {}", name);
+    info!("Applying Nextcloud elements: {}", name);
+    let deployments = vec![
+      "php-fpm",
+      "nginx",
+    ];
 
     let mut labels: BTreeMap<String, String> = BTreeMap::new();
     let mut annotations: BTreeMap<String, String> = BTreeMap::new();
     labels.insert("app".to_owned(), name.to_owned());
-    //TODO: use a real hashing function
-    let state_hash = create_hash(name, nextcloud_object.spec.replicas,
-        nextcloud_object.spec.php_image.clone(),
-        nextcloud_object.spec.nginx_image.clone());
-    annotations.insert("state_hash".to_owned(), state_hash.to_owned());
+
     info!("---- PULL SECRET? {:?}", nextcloud_object.spec.image_pull_secret.clone());
     let image_pull_secrets = vec![
         LocalObjectReference {
             name: Some(nextcloud_object.spec.image_pull_secret.clone()),
         },
     ];
-    /*TODO: check if we can get it as mutable nextcloud_object.metadata
-        .annotations.as_mut().unwrap()
-        .insert("state_hash".to_owned(), state_hash.to_owned());
-    */
 
-    // Definition of the deployment. Alternatively, a YAML representation could be used as well.
-    let deployment: Deployment = Deployment {
-        metadata: ObjectMeta {
-            name: Some(name.to_owned()),
-            namespace: Some(namespace.to_owned()),
-            labels: Some(labels.clone()),
-            annotations: Some(annotations),
-            ..ObjectMeta::default()
+    let nextcloud_elements = vec![
+        NextcloudElement {
+            name: "php-fpm".to_string(),
+            prefix: "php-fpm".to_string(),
+            image: nextcloud_object.spec.php_image.clone(),
+            namespace: namespace.to_string(),
+            replicas: nextcloud_object.spec.replicas, //TODO: should we use different replica size for each deployment?
+            container_port: 9000,
+            labels: labels.clone(),
+            image_pull_secrets: image_pull_secrets.clone(),
+            annotations: annotations.clone(),
         },
-        spec: Some(DeploymentSpec {
-            replicas: Some(nextcloud_object.spec.replicas),
-            selector: LabelSelector {
-                match_expressions: None,
-                match_labels: Some(labels.clone()),
-            },
-            template: PodTemplateSpec {
-                spec: Some(PodSpec {
-                    containers: vec![
-                        Container {
-                            name: format!("php-fpm-{}",name).to_string(),
-                            image: Some(nextcloud_object.spec.php_image.clone()),
-                            ports: Some(vec![ContainerPort {
-                                container_port: 9000,
-                                ..ContainerPort::default()
-                            }]),
-                        ..Container::default()
-                        },
-                        Container {
-                            name: format!("nginx-{}",name).to_string(),
-                            image: Some(nextcloud_object.spec.nginx_image.clone()),
-                            ports: Some(vec![ContainerPort {
-                                container_port: 80,
-                                ..ContainerPort::default()
-                            }]),
-                        ..Container::default()
-                        },
-                    ],
-                    image_pull_secrets: Some(image_pull_secrets),
-                    ..PodSpec::default()
-                }),
-                metadata: Some(ObjectMeta {
-                    labels: Some(labels),
-                    ..ObjectMeta::default()
-                }),
-            },
-            ..DeploymentSpec::default()
-        }),
-        ..Deployment::default()
-    };
+        NextcloudElement {
+            name: "nginx".to_string(),
+            prefix: "nginx".to_string(),
+            image: nextcloud_object.spec.nginx_image.clone(),
+            namespace: namespace.to_string(),
+            replicas: nextcloud_object.spec.replicas,
+            container_port: 80,
+            labels: labels.clone(),
+            image_pull_secrets: image_pull_secrets.clone(),
+            annotations: annotations.clone(),
+        },
+    ];
 
-    // let ssapply = PatchParams::apply("crd_apply_example").force();
     let patch_params = PatchParams {
         field_manager: Some("nextcloud_field_manager".to_string()),
         ..PatchParams::default()
     };
-    // Create the deployment defined above
-    let deployment_api: Api<Deployment> = Api::namespaced(client, namespace);
-    let ret = deployment_api
-        //.create(&PostParams::default(), &deployment)
-        .patch(name, &patch_params, &Patch::Apply(&deployment))
-        .await;
 
-        //foos.patch("baz", &ssapply, &Patch::Apply(&foo)).await?;
-    info!("Done Deploying: {}", name);
-    ret
+    for dep in nextcloud_elements {
+        let state_hash = create_hash(
+            name,
+            dep.replicas,
+            dep.image.clone()
+        );
+        annotations.insert("state_hash".to_owned(), state_hash);
+        let deployment = dep.as_deployment()?;
+
+        // Create the deployment defined above
+        let deployment_api: Api<Deployment> = Api::namespaced(client.clone(), namespace);
+        let ret = deployment_api
+            .patch(name, &patch_params, &Patch::Apply(&deployment))
+            .await;
+        info!("Done Deploying: {}", dep.name);
+    }
+
+    Ok(())
 }
 
 /// Deletes an existing deployment.
@@ -139,14 +177,19 @@ pub async fn delete(client: Client, name: &str, namespace: &str) -> Result<(), E
 
 
 /// Creates a sha256 hash from the given attributes
-pub fn create_hash(name: &str, replicas: i32, php_image: String,
-    nginx_image: String) -> String {
-    let state_string = format!("{}-{}-{}-{}", name, replicas.to_string(),
-				php_image, nginx_image);
-		let mut hasher = Sha256::new();
-		hasher.update(state_string.as_bytes());
-		hasher.finalize()
-			.iter()
-			.map(|byte| format!("{:02x}", byte))
-			.collect::<String>()
+pub fn create_hash(name: &str,
+    replicas: i32,
+    image: String,
+) -> String {
+    let state_string = format!("{}-{}-{}",
+        name,
+        replicas.to_string(),
+        image
+    );
+    let mut hasher = Sha256::new();
+    hasher.update(state_string.as_bytes());
+    hasher.finalize()
+      .iter()
+      .map(|byte| format!("{:02x}", byte))
+      .collect::<String>()
 }
