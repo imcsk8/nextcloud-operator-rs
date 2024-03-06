779e1097c697b6cb2e914066e6ba146d89873a6a
Iván Chavero <ichavero@chavero.com.mx>
2024-03-06 01:22:53 -0600

    Update instruction for secret creation

--- utils/create_registry_secret.sh
+++ utils/create_registry_secret.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# for podman:
+# ${XDG_RUNTIME_DIR}/containers/auth.json
+# for docker:
+# $HOME/.docker/config.json
+
 kubectl create secret generic $1     \
     -n $3                            \
     --from-file=.dockerconfigjson=$2 \
