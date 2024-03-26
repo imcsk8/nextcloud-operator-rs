47488b5c1da53846d2f0dd9b903c414104e2d567
Iván Chavero <ichavero@chavero.com.mx>
2024-03-25 23:15:36 -0600

    Add error module

---
+++ src/error.rs
@@ -0,0 +1,20 @@
+use kube::{Error};
+use thiserror::Error;
+
+/// All errors possible to occur during reconciliation
+#[derive(Debug, Error)]
+pub enum NextcloudError {
+    /// Any error originating from the `kube-rs` crate
+    #[error("Kubernetes reported error: {source}")]
+    KubeError {
+        #[from]
+        source: kube::Error,
+    },
+    /// Error in user input or Nextcloud resource definition, typically missing fields.
+    #[error("Invalid Nextcloud CRD: {0}")]
+    UserInputError(String),
+    #[error("Deploy Nextcloud Error: {0}")]
+    DeployError(String),
+}
+
+
